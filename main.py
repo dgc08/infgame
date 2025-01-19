e091ebf1d771c000bdc21e38e780bef940eaaebf
Sinthoras39 <digc0820@gmail.com>
2025-01-19 14:56:06 +0100

    Scale stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from random import randint
+from pygame.math import Vector2
 import pgnull
 
 game = pgnull.Game()
@@ -10,66 +11,76 @@ HEIGHT = 400
 
 game.open_screen(WIDTH, HEIGHT)
 
-fox = pgnull.Sprite("fox", scale=(0.2,0.2))
-fox.pos = 100, 100
+fox = pgnull.Sprite("images/fox.png", scale=(0.2,0.2), pos=(50,50))
+#fox.pos = (50,50)
 
-coin = pgnull.Sprite("coin")
+coin = pgnull.Sprite("images/coin.png", scale=(0.2,0.2))
 
 score = 0
 game_over = False
 
 def time_up():
-    global game_over
+    global game_over, fox, coin
+    
     game_over = True
+    del fox
+    del coin
 
 def place_coin():
-    coin.x = randint(20, (WIDTH-20))
-    coin.y = randint(20, (HEIGHT-20))
+    # why doesn't python has do-while?
+    loop = True
+    while loop:
+        coin.x = randint(20, (WIDTH-20))
+        coin.y = randint(20, (HEIGHT-20))
+        loop = fox.colliderect(coin)
 
 def draw():
-    game.screen.fill(pgnull.Colors.GREEN)
-    game.screen.draw_text("Punkte: " + str(score), color="black", topleft=(10, 10))
-
-    coin.draw()
-    fox.draw()
-
     if game_over:
         game.screen.fill("pink")
         game.screen.draw_text("Endstand: " + str(score), topleft=(10, 10), fontsize=60)
         game.screen.draw_text("Noch mal spielen? (j/n)", topleft=(10, 100), fontsize=40)
+    else:
+        game.screen.fill(pgnull.Colors.GREEN)
+        game.screen.draw_text("Punkte: " + str(score), color="black", topleft=(10, 10))
+
+        coin.draw()
+        fox.draw()
 
 def update(context):
     global score
     global game_over
 
-    if context.keyboard.left:
-        fox.x = fox.x - 2
-    elif context.keyboard.right:
-        fox.x = fox.x + 2
-    if context.keyboard.up:
-        fox.y = fox.y - 2
-    elif context.keyboard.down:
-        fox.y = fox.y + 2
-
-    coin_collected = fox.colliderect(coin)
-    if coin_collected:
-        score += 10
-        place_coin()
-
     if game_over and context.keyboard.j:
         game_over = False
         score = 0
-
-    if game_over and context.keyboard.n:
+    elif game_over and context.keyboard.n:
         game.quit()
+    elif game_over:
+        pass
+    else:
+        fox.scale = Vector2(0.2, 0.2)
+        if context.keyboard.left:
+            fox.rotation += 5
+            fox.x = fox.x - 2
+        elif context.keyboard.right:
+            fox.rotation -= 5
+            fox.x = fox.x + 2
+        if context.keyboard.up:
+            fox.y = fox.y - 2
+            fox.scale = Vector2(fox.scale.x, 0.4)
+        if context.keyboard.down:
+            fox.scale = Vector2(fox.scale.x, 0.1)
+            fox.y = fox.y + 2
+
+
+        if fox.colliderect(coin):
+            score += 10
+            print(fox.pos)
+            place_coin()
 
     draw()
 
-def on_exit():
-    print("i am closnngig")
-
 place_coin()
 game.clock.schedule(time_up, 15.0)
-game.register_event("on_close", on_exit)
 
 game.run_game(update)
