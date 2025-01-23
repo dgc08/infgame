6403e7a5f8e694dd72e1ba6b5d8412fcf52add4c
Sinthoras39 <digc0820@gmail.com>
2025-01-23 22:02:55 +0100

    added scenes classes to example game

--- main.py
+++ main.py
@@ -7,95 +7,96 @@ import pgnull
 WIDTH = 600
 HEIGHT = 600
 
-lobby = pgnull.Scene("dim grey")
-main = pgnull.Scene("green")
-
-### Lobby
 game = pgnull.Game(WIDTH, HEIGHT, "some game")
 
-box_w = Vector2(180, 150)
+#lobby = pgnull.Scene("dim grey")
+#main = pgnull.Scene("green")
 
-title_box= pgnull.TextBox((300,100), "Some random ass game")
-lobby.add_game_object(title_box)
+### Lobby
 
-start_game_button = pgnull.Button(((WIDTH-box_w.x)/2, 160), "Start", box_w, "orange")
-@start_game_button.event("on_click")
-def on_click():
-    game.load_scene(main)
+class Lobby(pgnull.Scene):
+    box_w = Vector2(180, 150)
 
-lobby.add_game_object(start_game_button)
+    title_box= pgnull.TextBox("Some random game", (300,100))
+    start_game_button = pgnull.Button("Start", ((WIDTH-box_w.x)/2, 160), box_w, "orange")
+    exit_button = pgnull.Button( "Quit", ((WIDTH-box_w.x)/2, start_game_button.y+start_game_button.width+5), box_w, "orange")
 
-exit_button = pgnull.Button(((WIDTH-box_w.x)/2, start_game_button.y+start_game_button.width+5), "Quit", box_w, "orange")
-@exit_button.event("on_click")
-def on_click():
-    game.quit()
+    @start_game_button.event("on_click")
+    def on_click():
+        game.load_scene(Main())
 
-lobby.add_game_object(exit_button)
+    @exit_button.event("on_click")
+    def on_click():
+        game.quit()
 
-#game.run_game(lobby)
-
-### REAL GAME
-
-player = pgnull.Sprite("images/player.png", scale=0.2, pos=(50,50))
-coin = pgnull.Sprite("images/coin.png", scale=0.5)
-
-main.add_game_object(player)
-main.add_game_object(coin)
-
-score = 0
-game_over = False
-
-def time_up():
-    global game_over, player, coin
-
-    game_over = True
-    player.active = False
-    coin.active = False
-
-def place_coin_and_score(points=10):
-    global score
-    # why doesn't python have do-while?
-    loop = 10
-    while loop:
-        coin.x = randint(0, WIDTH-coin.width)
-        coin.y = randint(0, HEIGHT-coin.height)
-
-        if player.colliderect(coin):
-            if loop == 1:
-                return
-            loop -= 1
-        else:
-            break
+    def __init__(self):
+        super().__init__("dim grey")
+        self.add_game_object(Lobby.title_box)
+        self.add_game_object(Lobby.start_game_button)
+        self.add_game_object(Lobby.exit_button)
 
 
-    score += points
 
-@main.event("on_predraw")
-def predraw():
-    if game_over:
-        game.screen.fill("pink")
-        game.screen.draw_text("Endstand: " + str(score), topleft=(10, 10), fontsize=60)
-        game.screen.draw_text("Noch mal spielen? (j/n)", topleft=(10, 100), fontsize=40)
-    else:
-        game.screen.draw_text("Punkte: " + str(score), color="black", topleft=(10, 10))
+class GameOverScreen(pgnull.Scene):
+    def __init__(self):
+        super().__init__("pink")
 
+        self._game_objs = [pgnull.TextBox("Endstand: " + str(Main.score), topleft=(10, 10), fontsize=60),
+                           pgnull.TextBox("Noch mal spielen? (j/n)", topleft=(10, 100), fontsize=40)
+                           ]
+
+    @staticmethod
+    def on_update(ctx):
+        if ctx.keyboard.j:
+            game.load_scene(Main())
+        elif ctx.keyboard.n:
+            game.quit()
         
 
-@main.event("on_update")
-def update(context):
-    global score
-    global game_over
+### REAL GAME
+class Main(pgnull.Scene):
+    score = None
 
-    if game_over and context.keyboard.j:
-        game_over = False
-        player.active = True
-        coin.active = True
-        score = 0
-    elif game_over and context.keyboard.n:
-        game.quit()
-    elif game_over:
-        pass
-    else:
+    player = pgnull.Sprite("images/player.png", scale=0.2, pos=(50,50))
+    coin = pgnull.Sprite("images/coin.png", scale=0.5)
+
+    score_label = pgnull.TextBox(topleft=(10, 10), text="Punkte: "+str(score), color="black")
+
+    def __init__(self):
+        super().__init__("green")
+
+        self._game_objs = [Main.player,
+                           Main.coin,
+                           Main.score_label]
+        Main.score = 0
+
+    def time_up():
+        game.load_scene(GameOverScreen())
+
+    def place_coin_and_score(points=10):
+        # why doesn't python have do-while?
+        loop = 10
+        while loop:
+            Main.coin.x = randint(0, WIDTH-Main.coin.width)
+            Main.coin.y = randint(0, HEIGHT-Main.coin.height)
+
+            if Main.player.colliderect(Main.coin):
+                if loop == 1:
+                    return
+                loop -= 1
+            else:
+                break
+
+
+        Main.score += points
+
+    @staticmethod
+    def on_update(context):
+        player = Main.player
+        coin = Main.coin
+
+        Main.score_label.text = "Punkte: "+str(Main.score)
+
         player.scale = Vector2(0.2)
         if context.keyboard.left:
             player.rotation += 5
@@ -115,11 +116,11 @@ def update(context):
 
 
         if player.colliderect(coin):
-            place_coin_and_score()
-        
-@main.event("on_start")
-def on_start():
-    place_coin_and_score(0)
-    game.clock.schedule(time_up, 15.0)
+            Main.place_coin_and_score()
+    @staticmethod
+    def on_start():
+        Main.place_coin_and_score(0)
+        game.clock.schedule(Main.time_up, 15.0)
 
-game.run_game(lobby)
+
+game.run_game(Lobby())
