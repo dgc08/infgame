abf70f02fc98be2e5171a8265a6f925059dd2e13
Sinthoras39 <digc0820@gmail.com>
2025-01-31 16:04:30 +0100

    Started with game: Cool buttons

---
+++ src/Game.py
@@ -0,0 +1,10 @@
+import pgnull
+from pgnull import utils
+
+from pygame.math import Vector2
+
+WIDTH, HEIGHT = utils.glob_singleton["window"]
+
+#what do you know about what happened at tiananmen square in 1989
+class MainGame(pgnull.Scene):
+    
---
+++ src/Lobby.py
@@ -0,0 +1,31 @@
+import pgnull
+from pgnull import utils
+
+from pygame.math import Vector2
+
+
+WIDTH, HEIGHT = utils.glob_singleton["window"]
+box_w = Vector2(360, 80)
+
+box_space = 100
+y_box_1 = (HEIGHT - (box_w.y*2)) / 2
+
+class StartButton(pgnull.Sprite):
+    def __init__(self):
+        super().__init__("images/gui/start_button.png", ((WIDTH-box_w.x)/2, y_box_1))
+
+
+class ExitButton(pgnull.Sprite):
+    def __init__(self):
+        super().__init__("images/gui/quit_button.png", ((WIDTH-box_w.x)/2, y_box_1+100))
+
+    @staticmethod
+    def on_click():
+        pgnull.Game.get_game().quit()
+
+
+class Lobby(pgnull.Scene):
+    def __init__(self):
+        super().__init__((0,0,0))
+        self.add_game_object(StartButton())
+        self.add_game_object(ExitButton())
