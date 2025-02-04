2cef723c18d908cd7a2e6f0271526f51692c59dc
Sinthoras39 <digc0820@gmail.com>
2025-02-04 19:59:05 +0100

    camera

--- src/Game.py
+++ src/Game.py
@@ -10,4 +10,12 @@ WIDTH, HEIGHT = utils.glob_singleton["window"]
 class MainGame(pgnull.Scene):
     def __init__(self):
         super().__init__((0,0,0))
-        self.add_game_object(Player())
+
+        player = Player()
+        player.pos = (150,100)
+
+        self.add_game_object(pgnull.Sprite("images/bg.png", scale=10))
+
+        self.add_game_object(pgnull.Sprite("images/cook.png", pos=(150,200), scale=0.2))
+
+        self.add_game_object(player)
--- src/Player.py
+++ src/Player.py
@@ -10,9 +10,11 @@ from .Bullet import Bullet
 SPEED = 2
 BULLET_SPEED = 8
 
+screen_size = Vector2(pgnull.utils.glob_singleton["window"])
+
 class Player(pgnull.Sprite):
     def __init__(self):
-        super().__init__("images/player.png")
+        super().__init__("images/player.png", pivot=(0,0))
         self.rotation = 90
 
     def update(self, ctx):
@@ -26,14 +28,21 @@ class Player(pgnull.Sprite):
         if ctx.keyboard.down or ctx.keyboard.s:
             self.y = self.y + SPEED
 
+        pgnull.Game.get_game().scene.camera = self.center - screen_size/2
+
+        if ctx.keyboard.e:
+            self.rotation -= 1
+        if ctx.keyboard.q:
+            self.rotation += 1
+
         # look at mouse
         mpos = Vector2(mouse.get_pos())
 
-        angle = Vector2(0,0).angle_to(mpos-self.pos)
+        angle = Vector2(0,0).angle_to(mpos-self.pos + pgnull.Game.get_game().scene.camera)
         self.rotation = (-1*angle)+180
 
         # shoot
         if ctx.keyboard.space:
-            bullet = Bullet(Vector2(BULLET_SPEED).rotate(angle-45), self.pos)
+            bullet = Bullet(Vector2(BULLET_SPEED).rotate(angle-45), self.center)
             bullet.rotation = -(angle)-90
             pgnull.Game.get_game().scene.add_game_object(bullet)
