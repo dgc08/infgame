45250a099959a300d8d2c984d49008474534e796
Sinthoras39 <digc0820@gmail.com>
2025-05-25 16:46:52 +0200

    start gen

--- src/Game.py
+++ src/Game.py
@@ -11,11 +11,3 @@ class MainGame(pgnull.Scene):
     def __init__(self):
         super().__init__((0,0,0))
 
-        player = Player()
-        player.pos = (150,100)
-
-        self.add_game_object(pgnull.Sprite("images/bg.png", scale=10))
-
-        self.add_game_object(pgnull.Sprite("images/cook.png", pos=(150,200), scale=0.2))
-
-        self.add_game_object(player)
